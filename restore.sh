d1001f48eea827de6185c1f77a7675610be7c8a5
winterdawn <selah@outlook.com>
2015-08-11 23:29:43 +0800

    fix path issue

--- restore.sh
+++ restore.sh
@@ -1,4 +1,5 @@
 cp bashrc ~/.bashrc
 cp vimrc ~/.vimrc
-cp syntax/sh.vim ~/.vim/syntax
-cp syntax/doxygen.vim ~/.vim/syntax
+mkdir ~/.vim/syntax/
+cp syntax/sh.vim ~/.vim/syntax/
+cp syntax/doxygen.vim ~/.vim/syntax/
